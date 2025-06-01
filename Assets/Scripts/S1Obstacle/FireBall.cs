2972930bcbd9922e5cb82c1ac628d32d3f3fba7c
unknown <pgh4131@gmail.com>
2025-06-01 22:41:42 +0900

    feat: add stage1 obstacle base script and prefab

--- Assets/Scripts/S1Obstacle/FireBall.cs
+++ Assets/Scripts/S1Obstacle/FireBall.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : Obstacle
+public class FireBall : Obstacle
 {
     protected override void OnHitPlayer()
     {
-        
+        //dotdeal
     }
 }
