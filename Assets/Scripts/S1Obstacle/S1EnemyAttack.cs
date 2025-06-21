718e8299b05146f08702304c5c93f7a921647957
박세희 <parksh050401@gmail.com>
2025-06-21 16:27:02 +0900

    base enemy prefab & stage1 enemy finished

--- Assets/Scripts/S1Obstacle/S1EnemyAttack.cs
+++ Assets/Scripts/S1Obstacle/S1EnemyAttack.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class S1EnemyAttack : Obstacle
 {
+    Animator anim;
+    BoxCollider box;
+    AnimatorStateInfo info;
+
+    void Start()
+    {
+        Debug.Log(transform.parent.name);
+        anim = transform.parent?.GetComponent<Animator>();
+        box = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        info = anim.GetCurrentAnimatorStateInfo(0);
+        if (info.shortNameHash == Animator.StringToHash("Attack") && info.normalizedTime > 0.4f && info.normalizedTime < 0.6f)
+        {
+            box.enabled = true;
+        }
+        else
+        {
+            box.enabled = false;
+        }
+    }
+
     protected override void OnHitPlayer()
     {
         //dotdeal
